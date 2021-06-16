cc74bcb69500303e20d63d50054c8f393a9d6b10
Anil Kumar <pangaanilkumar1@gmail.com>
2021-06-16 13:54:23 +0530

    Just for learning did the changes

--- inetBankingV1/src/test/java/com/inetBankings/testcases/TC_verify_001.java
+++ inetBankingV1/src/test/java/com/inetBankings/testcases/TC_verify_001.java
@@ -7,7 +7,7 @@ import com.inetBankings.pageobject.LoginPage;
 public class TC_verify_001 {
 @Test 
 public void login() {
-	LoginPage lg=new LoginPage("chrome");
+	LoginPage lg=new LoginPage();
 	
 	
 }
