f9eadb5ce975a29d6ef54191deef101df8cc354e
Anil Kumar <pangaanilkumar1@gmail.com>
2021-06-16 13:58:36 +0530

    Verification is done

--- inetBankingV1/src/test/java/com/inetBankings/testcases/TC_verify_001.java
+++ inetBankingV1/src/test/java/com/inetBankings/testcases/TC_verify_001.java
@@ -2,13 +2,14 @@ package com.inetBankings.testcases;
 
 import org.testng.annotations.Test;
 
+import com.inetBankings.genaric.lib.WebDrivercommonlib;
 import com.inetBankings.pageobject.LoginPage;
 
 public class TC_verify_001 {
 @Test 
-public void login() {
+public void login(String expected) {
 	LoginPage lg=new LoginPage();
-	
-	
+	WebDrivercommonlib wb=new WebDrivercommonlib();
+	wb.verify(wb.getTitle(),expected);
 }
 }
